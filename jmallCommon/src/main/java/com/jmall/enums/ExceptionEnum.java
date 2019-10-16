3501640f8b0fa3789d33c27827067950581d7c94
jumping <632741793@qq.com>
2019-10-16 23:01:25 +0800

    fix : 修复上传文件错误问题

--- jmallCommon/src/main/java/com/jmall/enums/ExceptionEnum.java
+++ jmallCommon/src/main/java/com/jmall/enums/ExceptionEnum.java
@@ -13,10 +13,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public enum ExceptionEnum {
-    CATEGORY_NOT_FOUND(404,"商品分类未找到！"),
-    BRAND_NOT_FOUND(404,"品牌未找到！"),
-    BRAND_SAVE_ERROR(500,"新增品牌失败!"),
-    IMAGE_UPLOAD_ERROR(500,"上传图片失败!")
+    CATEGORY_NOT_FOUND(404,"商品分类未找到"),
+    BRAND_NOT_FOUND(404,"品牌未找到"),
+    BRAND_SAVE_ERROR(500,"新增品牌失败"),
+    FILE_UPLOAD_ERROR(500,"上传文件失败"),
+    INVALID_FILE_TYPE(500,"无效的文件类型")
     ;
     private int code;
     private String msg;
--- jmallUpload/src/main/java/com/jamll/service/UploadService.java
+++ jmallUpload/src/main/java/com/jamll/service/UploadService.java
@@ -3,9 +3,12 @@ package com.jamll.service;
 import com.jmall.enums.ExceptionEnum;
 import com.jmall.exception.JmallException;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
@@ -19,10 +22,19 @@ import java.util.List;
 @Service
 @Slf4j
 public class UploadService {
-    private static final List<String> types = Arrays.asList("image/png","image/jpeg","image/bmp");
+    private static final List<String> ALLOW_TYPES = Arrays.asList("image/png","image/jpeg","image/bmp");
     public String uploadImage(MultipartFile file) {
         try {
+            //检验文件类型
             String contentType = file.getContentType();
+            if(!ALLOW_TYPES.contains(contentType)){
+                throw new JmallException(ExceptionEnum.INVALID_FILE_TYPE);
+            }
+            //检验文件内容
+            BufferedImage image = ImageIO.read(file.getInputStream());
+            if(image==null){
+                throw new JmallException(ExceptionEnum.INVALID_FILE_TYPE);
+            }
             //准备目标路径
             File dest = new File("F:\\IdeaWork\\upload\\",file.getOriginalFilename());
             //保存文件到本地
@@ -31,7 +43,7 @@ public class UploadService {
             return "http://image.jmall.com/"+file.getOriginalFilename();
         } catch (IOException e) {
             log.error("上传图片失败",e);
-            throw new JmallException(ExceptionEnum.IMAGE_UPLOAD_ERROR);
+            throw new JmallException(ExceptionEnum.FILE_UPLOAD_ERROR);
         } finally {
 
         }
